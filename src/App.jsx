fc8b6a98039e68ba83b7fc5b4c2d7166831361de
Alexis <isalexis.design@gmail.com>
2026-01-30 19:59:38 +0100

    Refactorizacion en componente GameZone separando responsabilidades. Refactorizacion y ampliacion de datos.json para hacer el código más legible

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useForm } from 'react-hook-form';
 import datos from './datos.json';
 import { useState } from 'react';
+import { GameZone } from './components/GameZone';
 
 function App() {
 
@@ -8,22 +8,15 @@ function App() {
     return [...datos].sort(() => Math.random() - 0.5)
   });
   
-  const {register, handleSubmit, reset, formState: {errors}} = useForm();
 
   const [contador, setContador] = useState(0);
 
   const [puntos, setPuntos] = useState(0);
 
-  const handleSubmitForm = handleSubmit(
-    data => {
-      if(data.nombreInput.toLowerCase().trim() === paises[contador].pais.toLowerCase()) setPuntos(puntos + 1)
-
-      setContador(contador+1);
-
-      reset();
-    }
-  );
-
+  function alTerminarTurno (resultado) {
+    setPuntos(puntos + resultado)
+    setContador(contador+1);
+  }
   
   if (contador >= 5){
     return(
@@ -34,32 +27,16 @@ function App() {
     )
   }
 
+  const paisActual = paises[contador];
+
   return (
     
     <>
       <h1>Diversión con banderas</h1>
-      <h3>Ronda {contador+1}</h3>
-        <form onSubmit={handleSubmitForm}>
-          <img src={paises[contador].url} alt={paises[contador].pais} />
-
-          Nombre del país:
-          <input 
-            type="text" 
-            autoFocus
-            {
-              ...register('nombreInput', {
-                required: '¡Escribe algo!',
-                minLength: {
-                  value: 2,
-                  message: '¡Prueba con un nombre más largo!'
-                }
-              })
-            }
-          />
-          <p> {errors.nombreInput?.message} </p>
-
-          <button type="submit" >Enviar</button>
-        </form>
+      <GameZone rondaActual={contador+1} 
+                paisActual={paisActual} 
+                alTerminarTurno={alTerminarTurno} >
+      </GameZone>
     </>
     
   )
---
+++ src/components/GameZone.jsx
@@ -0,0 +1,45 @@
+import { useForm } from "react-hook-form";
+
+export const GameZone = ({rondaActual, paisActual, alTerminarTurno}) => {
+
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+
+    const handleSubmitForm = handleSubmit(
+    data => {
+        let resultado = 0;
+
+        if(data.nombreInput.toLowerCase().trim() === paisActual.nombre.toLowerCase()) resultado = 1;
+      
+        alTerminarTurno(resultado);
+        
+        reset();
+    }
+  );
+
+  return (
+    <>
+      <h3>Ronda {rondaActual}</h3>
+        <form onSubmit={handleSubmitForm}>
+          <img src={paisActual.url} alt={paisActual.nombre} />
+
+          Nombre del país:
+          <input 
+            type="text" 
+            autoFocus
+            {
+              ...register('nombreInput', {
+                required: '¡Escribe algo!',
+                minLength: {
+                  value: 2,
+                  message: '¡Prueba con un nombre más largo!'
+                }
+              })
+            }
+          />
+          <p> {errors.nombreInput?.message} </p>
+
+          <button type="submit" >Enviar</button>
+        </form>
+    </>
+  )
+}
